ce3620fcadf8a78dc2431cb338d8c7da1ef4b9f0
bilalkarakollu <karakollubilal@gmail.com>
2022-05-02 20:54:38 +0300

    global style ler eklendi

--- src/styled/Global.ts
+++ src/styled/Global.ts
@@ -1,4 +1,5 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+import { RowProps } from "../types/global.styled";
 
 export const Global = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
@@ -11,4 +12,22 @@ export const Global = createGlobalStyle`
     body {
         font-family: 'Roboto', sans-serif; 
     }
+`;
+
+export const Container = styled.div`
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+`;
+
+export const Row = styled.div<RowProps>`
+    display: grid;
+    grid-template-columns: ${({templateColumns}) => templateColumns};
+    gap: ${({gap}) => gap}rem;
+`;
+
+export const Flex = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
---
+++ src/types/global.styled.ts
@@ -0,0 +1,4 @@
+export interface RowProps {
+    gap?: number;
+    templateColumns?: string;
+}
