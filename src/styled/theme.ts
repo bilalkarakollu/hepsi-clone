9f5dc7cc28907e51c875752f5209995189096b12
bilalkarakollu <karakollubilal@gmail.com>
2022-05-07 21:13:13 +0300

    theme değişkeni styled klasörünün altına alındı

---
+++ src/styled/theme.ts
@@ -0,0 +1,18 @@
+import { Theme } from "../types/theme";
+
+export const theme: Theme = {
+    media: {
+      xs: "only screen and (max-width: 576px)",
+      sm: "only screen and (min-width: 576px)",
+      md: "only screen and (min-width: 768px)",
+      lg: "only screen and (min-width: 992px)",
+      xl: "only screen and (min-width: 1200px)",
+    },
+    colors: {
+      theme: "#FF6000",
+      text: "#484848",
+      text_hover: "white",
+      background: "#FFFFFF",
+      hover: "#e8e8e8",
+    },
+  };
