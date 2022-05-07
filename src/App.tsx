189f387ae79bbe2d6d4b09bff5c3e6454cebc658
bilalkarakollu <karakollubilal@gmail.com>
2022-05-07 21:26:37 +0300

    test ortamı hazırlandı

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,48 @@
 import React from "react";
 import { Global } from "./styled/Global";
 import { ThemeProvider } from "styled-components";
-import { Theme } from "./types/theme";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import ProductDetay from "./pages/ProductDetay";
 import Sepet from "./pages/Sepet";
 import Favoriler from "./pages/Favoriler";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
+import { theme } from "./styled/theme";
 
 const App = () => {
-  const theme: Theme = {
-    media: {
-      xs: "only screen and (max-width: 576px)",
-      sm: "only screen and (min-width: 576px)",
-      md: "only screen and (min-width: 768px)",
-      lg: "only screen and (min-width: 992px)",
-      xl: "only screen and (min-width: 1200px)",
-    },
-    colors: {
-      theme: "#FF6000",
-      text: "#484848",
-      text_hover: "white",
-      background: "#FFFFFF",
-      hover: "#e8e8e8",
-    },
-  };
+  
   return (
-    <ThemeProvider theme={theme}>
-      <Global />
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index  element={<Home />} />
-          <Route path="/sepet"  element={<Sepet />} />
-          <Route path="/favoriler"  element={<Favoriler />} />
-          <Route path="/:id"  element={<ProductDetay />} />
-        </Route>
-      </Routes>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Global />
+          <ToastContainer
+            position="top-right"
+            autoClose={1500}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover={false}
+            limit={1}
+          />
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<Home />} />
+              <Route path="/sepet" element={<Sepet />} />
+              <Route path="/favoriler" element={<Favoriler />} />
+              <Route path="/:id" element={<ProductDetay />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,9 @@
 import React from "react";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-      <ToastContainer
-        position="top-right"
-        autoClose={1500}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover={false}
-        limit={1}
-      />
-    </Provider>
-  </BrowserRouter>
-);
+root.render(<App />);
